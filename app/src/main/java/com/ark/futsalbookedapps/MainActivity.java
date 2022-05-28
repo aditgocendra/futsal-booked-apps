2286f9257143cb8a6f344424a681eab9d2b6442c
aditgocendra <gocendra123@gmail.com>
2022-05-28 11:40:23 +0700

    routes update

--- app/src/main/java/com/ark/futsalbookedapps/MainActivity.java
+++ app/src/main/java/com/ark/futsalbookedapps/MainActivity.java
@@ -1,8 +1,15 @@
 package com.ark.futsalbookedapps;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.ark.futsalbookedapps.Globals.Data;
+import com.ark.futsalbookedapps.Globals.Functions;
+import com.ark.futsalbookedapps.Views.Auth.Authentication;
+import com.ark.futsalbookedapps.Views.Users.Home;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Functions.checkWindowSetFlag(this);
+
+        routes();
+    }
+
+    private void routes() {
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            if (firebaseUser != null){
+                Functions.updateUI(MainActivity.this, Home.class);
+                Data.uid = firebaseUser.getUid();
+            }else {
+                Functions.updateUI(MainActivity.this, Authentication.class);
+            }
+
+        }, 1000);
     }
 }

82b77980e99775f4b610dfa21162bbba99a780f9
aditgocendra <gocendra123@gmail.com>
2022-05-28 23:06:17 +0700

    add getter and preview location user

--- app/src/main/java/com/ark/futsalbookedapps/Globals/Functions.java
+++ app/src/main/java/com/ark/futsalbookedapps/Globals/Functions.java
@@ -3,11 +3,23 @@ package com.ark.futsalbookedapps.Globals;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.graphics.Color;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Build;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.tasks.Task;
 
 import java.text.NumberFormat;
 import java.util.Locale;
@@ -50,5 +62,52 @@ public class Functions {
             winParams.flags &= ~bits;
         }
         win.setAttributes(winParams);
+    }
+
+    public static boolean isGPSEnabled(Context mContext){
+        LocationManager locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+    public static void turnOnGPS(LocationRequest locationRequest, Activity activity) {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+        Task<LocationSettingsResponse> responseTask = LocationServices.getSettingsClient(activity).checkLocationSettings(builder.build());
+
+        responseTask.addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                try {
+                    task.getResult(ApiException.class);
+                    Toast.makeText(activity, "GPS IS ON", Toast.LENGTH_SHORT).show();
+                } catch (ApiException e) {
+                    switch (e.getStatusCode()) {
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                            try {
+                                ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                                resolvableApiException.startResolutionForResult(activity, 1005);
+                            } catch (IntentSender.SendIntentException ex) {
+                                ex.printStackTrace();
+                            }
+                            break;
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            Toast.makeText(activity, "Device do not have location", Toast.LENGTH_SHORT).show();
+                            break;
+                    }
+                }
+            } else {
+
+                Toast.makeText(activity, "GPS is off", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public static void previewStreetMaps(double latitude, double longitude, Context mContext){
+        String geo = "google.streetview:cbll="+latitude+","+longitude;
+        Uri gmmIntentUri = Uri.parse(geo);
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if (mapIntent.resolveActivity(mContext.getPackageManager()) != null) {
+            mContext.startActivity(mapIntent);
+        }
     }
 }

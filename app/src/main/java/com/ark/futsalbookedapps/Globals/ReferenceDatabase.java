fcba3f834af27f90dc3be52e3db60a4530f14fff
aditgocendra <gocendra123@gmail.com>
2022-05-28 23:09:19 +0700

    create global reference path database

---
+++ app/src/main/java/com/ark/futsalbookedapps/Globals/ReferenceDatabase.java
@@ -0,0 +1,11 @@
+package com.ark.futsalbookedapps.Globals;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ReferenceDatabase {
+    public static DatabaseReference referenceRoot = FirebaseDatabase.getInstance().getReference();
+    public static DatabaseReference referenceAccount = referenceRoot.child("account");
+    public static DatabaseReference referenceProviderField = referenceRoot.child("provider_field");
+    public static DatabaseReference referenceField = referenceRoot.child("field");
+}

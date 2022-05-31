47773ab95f6c30da62f7c3554d8ce0bba3fa4e29
aditgocendra <gocendra123@gmail.com>
2022-05-31 20:33:46 +0700

    feature filter data with status booked

--- app/src/main/java/com/ark/futsalbookedapps/Views/Users/FieldBooked.java
+++ app/src/main/java/com/ark/futsalbookedapps/Views/Users/FieldBooked.java
@@ -9,6 +9,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.ark.futsalbookedapps.Adapter.AdapterFieldBooked;
@@ -18,7 +20,10 @@ import com.ark.futsalbookedapps.Globals.Functions;
 import com.ark.futsalbookedapps.Globals.ReferenceDatabase;
 import com.ark.futsalbookedapps.Models.ModelBooked;
 import com.ark.futsalbookedapps.Models.ModelProviderField;
+import com.ark.futsalbookedapps.R;
 import com.ark.futsalbookedapps.databinding.ActivityFieldBookedBinding;
+import com.google.android.gms.dynamic.IFragmentWrapper;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.Query;
@@ -34,12 +39,19 @@ public class FieldBooked extends AppCompatActivity {
 
     // init adapter
     private AdapterFieldBooked adapterFieldBooked;
-    private List<ModelBooked> listBooked = new ArrayList<>();
+    private List<ModelBooked> listBooked;
 
     // param pagination data
     private long countData;
     private String key = null;
     private boolean isLoadData = false;
+
+    // Bottom Sheet Dialog
+    private BottomSheetDialog bottomSheetDialog;
+
+    // filter data
+    int status = 500;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +66,8 @@ public class FieldBooked extends AppCompatActivity {
 
         adapterFieldBooked = new AdapterFieldBooked(this);
         binding.recyclerFieldBooked.setAdapter(adapterFieldBooked);
+
+        listBooked = new ArrayList<>();
 
         listenerComponent();
         requestDataBooked();
@@ -88,6 +102,12 @@ public class FieldBooked extends AppCompatActivity {
             }
         });
 
+        bottomSheetDialog = new BottomSheetDialog(this);
+        setBottomDialogFilter();
+        binding.filterBtn.setOnClickListener(view -> {
+            bottomSheetDialog.show();
+        });
+
     }
 
     private void requestDataBooked(){
@@ -119,24 +139,36 @@ public class FieldBooked extends AppCompatActivity {
         query.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                binding.progressCircular.setVisibility(View.VISIBLE);
                 for (DataSnapshot ds : snapshot.getChildren()){
                     ModelBooked modelBooked = ds.getValue(ModelBooked.class);
                     if (modelBooked != null){
-                        modelBooked.setKeyBooked(ds.getKey());
-                        key = ds.getKey();
-                        listBooked.add(modelBooked);
+
+                        // filter condition
+                        if (status == 500){
+                            modelBooked.setKeyBooked(ds.getKey());
+                            listBooked.add(modelBooked);
+                            key = ds.getKey();
+                        }else {
+                            if (modelBooked.getStatus() == status){
+                                modelBooked.setKeyBooked(ds.getKey());
+                                listBooked.add(modelBooked);
+                                key = ds.getKey();
+                            }
+                        }
                     }
                 }
 
                 Handler handler = new Handler();
                 handler.postDelayed(() -> {
-                    adapterFieldBooked.setItem(listBooked);
                     if (listBooked.size() != 0){
-                        adapterFieldBooked.notifyDataSetChanged();
-                        isLoadData = false;
+                        adapterFieldBooked.setItem(listBooked);
                     }else {
-                        Toast.makeText(FieldBooked.this, "Boooked field provider is empty", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(FieldBooked.this, "Booked field provider is empty", Toast.LENGTH_SHORT).show();
                     }
+                    adapterFieldBooked.notifyDataSetChanged();
+                    isLoadData = false;
+                    binding.progressCircular.setVisibility(View.GONE);
                 }, 200);
             }
 
@@ -145,5 +177,63 @@ public class FieldBooked extends AppCompatActivity {
                 Toast.makeText(FieldBooked.this, error.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void setBottomDialogFilter(){
+        View viewBottomDialog = getLayoutInflater().inflate(R.layout.layout_filter_booked, null, false);
+
+        Button waitingPaid, cancelled, dpPay, bookedFinish;
+        waitingPaid = viewBottomDialog.findViewById(R.id.waiting_paid_btn);
+        cancelled = viewBottomDialog.findViewById(R.id.cancelled_btn);
+        dpPay = viewBottomDialog.findViewById(R.id.dp_pay_btn);
+        bookedFinish = viewBottomDialog.findViewById(R.id.booked_finish_btn);
+
+        waitingPaid.setOnClickListener(view -> {
+            waitingPaid.setEnabled(false);
+            cancelled.setEnabled(true);
+            dpPay.setEnabled(true);
+            bookedFinish.setEnabled(true);
+            filterData(0);
+            bottomSheetDialog.dismiss();
+        });
+
+        cancelled.setOnClickListener(view -> {
+            cancelled.setEnabled(false);
+            waitingPaid.setEnabled(true);
+            dpPay.setEnabled(true);
+            bookedFinish.setEnabled(true);
+            filterData(101);
+            bottomSheetDialog.dismiss();
+        });
+
+        dpPay.setOnClickListener(view -> {
+            dpPay.setEnabled(false);
+            waitingPaid.setEnabled(true);
+            cancelled.setEnabled(true);
+            bookedFinish.setEnabled(true);
+            filterData(202);
+            bottomSheetDialog.dismiss();
+        });
+
+        bookedFinish.setOnClickListener(view -> {
+            bookedFinish.setEnabled(false);
+            waitingPaid.setEnabled(true);
+            cancelled.setEnabled(true);
+            dpPay.setEnabled(true);
+            filterData(303);
+            bottomSheetDialog.dismiss();
+        });
+
+        bottomSheetDialog.setContentView(viewBottomDialog);
+
+    }
+
+    private void filterData(int codeStatus){
+        status = codeStatus;
+        key = null;
+        listBooked.clear();
+        isLoadData = true;
+        setDataFieldBooked();
+
     }
 }
